18cc56698d99f52279e6201ec494def67986e07e
maxwesslock <maxwellwesslock@gmail.com>
2026-06-02 07:42:43 -0400

    use railway volume for data persistence

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const DATA_FILE = path.join(__dirname, 'data.json');
+const DATA_FILE = process.env.RAILWAY_VOLUME_MOUNT_PATH
+  ? path.join(process.env.RAILWAY_VOLUME_MOUNT_PATH, 'data.json')
+  : path.join(__dirname, 'data.json');
 
 app.use(cors());
 app.use(express.json());
